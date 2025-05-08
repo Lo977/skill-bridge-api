a60bc6e2d45da0e2dc0579b85eca1b94cb1a7d80
Buddha Lo <lo_977@yahoo.com>
2025-05-07 23:22:20 -0700

    add signup resource

--- server/app.py
+++ server/app.py
@@ -15,11 +15,32 @@ from models import User, SkillOffer, SkillMatch,Skill
 CORS(app)
 
 # Views go here!
-
-@app.route('/')
-def index():
-    return '<h1>Project Server WELCOME</h1>'
-
+@app.before_request
+def check_if_logged_in():
+    public_routes=["/login","/signup","/check_session"]
+    if request.path not in public_routes and not session.get("user_id"):
+        return {"error":"Unauthorized"},401
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server WELCOME</h1>'
+'''------------------------------------ Signup --------------------------------------'''
+class SignupResource(Resource):
+    def post(self):
+        data = request.get_json()
+        if User.query.filter_by(username = data['username']).first():
+            return {"error":"Username already exists"},401
+        user = User(
+            username = data['username'],
+            email = data['email']
+        )
+        user.password_hash = data['password']
+        try:
+            db.session.add(user)
+            db.session.commit() 
+            return user.to_dict(),201
+        except Exception:
+            return {"error":"422 Unprocessable Entity"}, 422
+'''------------------------------------ UserResource --------------------------------------'''
 class UserResource(Resource):
     def get(self, id=None):
         if id:
@@ -28,7 +49,9 @@ class UserResource(Resource):
                 return user.to_dict(), 200
             return {'error': 'User not found'}, 404
         return [user.to_dict() for user in User.query.all()], 200
+    
 
+'''------------------------------------ SkillResource --------------------------------------'''
 class SkillResource(Resource):
     def get(self, id =None):
         if id is not None:
@@ -37,7 +60,8 @@ class SkillResource(Resource):
                 return skill.to_dict(),200
             return {"errors":"Skill not found!"}
         return [skill.to_dict() for skill in Skill.query.all()],200
-    
+
+'''------------------------------------ OfferResource --------------------------------------'''  
 class OfferResource(Resource):
     def get(self,id=None):
         if id is not None:
@@ -46,7 +70,9 @@ class OfferResource(Resource):
                 return offer.to_dict(),200
             return {"error":"Offer not found!"} 
         return [offer.to_dict() for offer in SkillOffer.query.all()],200
+    
 
+'''------------------------------------ MatchResource --------------------------------------'''  
 class MatchResource(Resource):
     def get(self,id=None):
         if id is not None:
@@ -55,11 +81,14 @@ class MatchResource(Resource):
                 return match.to_dict(),200
             return {"error":"Match not found!"} 
         return [match.to_dict() for match in SkillMatch.query.all()],200
+    
 
+'''------------------------------------ Routes --------------------------------------'''  
 api.add_resource(UserResource, '/users', '/users/<int:id>')
 api.add_resource(SkillResource,'/skills','/skills/<int:id>')
 api.add_resource(OfferResource,'/offers','/offers/<int:id>')
 api.add_resource(MatchResource,'/matches','/matches/<int:id>')
+api.add_resource(SignupResource,'/signup')
 
 
 if __name__ == '__main__':
