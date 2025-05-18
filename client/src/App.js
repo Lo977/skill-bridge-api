51415c73197516ee0681d6ff97d5d3e0d74cfa3d
Buddha Lo <lo_977@yahoo.com>
2025-05-17 22:55:11 -0700

    fetch check_session

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+  console.log(user);
+  useEffect(() => {
+    fetch("/check_session").then((res) => {
+      if (res.ok) {
+        res.json().then((userData) => setUser(userData));
+      }
+    });
+  }, []);
   return (
     <>
       <h2>App</h2>
