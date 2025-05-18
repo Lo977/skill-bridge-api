f0e254a584cf7be7dd00658d86bdfe8fb4f30b60
Buddha Lo <lo_977@yahoo.com>
2025-05-18 00:50:49 -0700

    create AppRoutes.js for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+import AppRoutes from "./AppRoutes";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -16,9 +17,10 @@ function App() {
   return (
     <>
       <Navbar onLogout={setUser} user={user} />
-      <Login onLogin={setUser} />
-      <Signup onLogin={setUser} />
+      {/* <Login onLogin={setUser} />
+      <Signup onLogin={setUser} /> */}
       <h2>App</h2>
+      <AppRoutes user={user} onLogin={setUser} />
     </>
   );
 }
---
+++ client/src/AppRoutes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function AppRoutes() {
+  return <div>AppRoutes</div>;
+}
+
+export default AppRoutes;
